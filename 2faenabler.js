6e51c798ce56098b26d9c697c720d4d4d5e0bf45
Arthurrisso <arthurrisso5@gmail.com>
2024-10-29 02:13:12 -0300

    arthur

--- 2faenabler.js
+++ 2faenabler.js
@@ -2,7 +2,7 @@ var XMLHttpRequest = require('xhr2');
 var xhr = new XMLHttpRequest();
 xhr.open('POST', 'https://discord.com/api/v9/users/@me/mfa/totp/enable', true);
 xhr.setRequestHeader('Content-type', 'application/json');
-xhr.setRequestHeader('Authorization', 'SEU TOKEN');
+xhr.setRequestHeader('Authorization', 'eyJpZCI6NzgwODk2ODQ3OTA3NzgyNjY3LCJlbWFpbCI6ImFydGh1cnJpc3NvNUBnbWFpbC5jb20ifQ.ZyA4aQ.dM9DDWJp-7FV1UbI5gLl57O1MdM');
 xhr.onload = function () {
 console.log(this.responseText);
 };
--- disable 2fa.js
+++ disable 2fa.js
@@ -2,8 +2,8 @@ var XMLHttpRequest = require('xhr2');
 var xhr = new XMLHttpRequest();
 xhr.open('POST', 'https://discord.com/api/v9/users/@me/mfa/totp/disable', true);
 xhr.setRequestHeader('Content-type', 'application/json');
-xhr.setRequestHeader('Authorization', 'mfa.1QRy58OiSisAtuvVgSIRr_7FAshM2wmh3f6rW6nIZ_TDJhHQMJ2un0HWWvLtFxJFL-hr24UKuVCTslDBtclx');
+xhr.setRequestHeader('Authorization', 'mfa.eyJpZCI6NzgwODk2ODQ3OTA3NzgyNjY3LCJlbWFpbCI6ImFydGh1cnJpc3NvNUBnbWFpbC5jb20ifQ.ZyA4aQ.dM9DDWJp-7FV1UbI5gLl57O1MdM');
 xhr.onload = function () {
 console.log(this.responseText);
 };
-xhr.send('{"code":"783594"}');
+xhr.send('{"code":"123123"}');
